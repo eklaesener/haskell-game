90cd5dab1e1c3ed0d1a22aab7805c3a717cecb3f
Elias Klaesener <eklaesener@techfak.de>
2018-12-06 15:40:54 +0100

    Added Character.hs

---
+++ Character.hs
@@ -0,0 +1,22 @@
+module Character where
+
+type Item = (Int, String, Int, [Int]) -- reads as (unique id, name, price, [possibly other attributes like power])
+
+data Character = Character { id :: Integer
+                           , name :: String
+                           , hp :: Float
+                           , pc :: Bool
+                           , items :: [Item]
+                           } deriving Show
+
+-- We don't need every value to be the same, only the unique id, so we can't derive Eq
+instance Eq Character where
+   (Character idA _ _ _ _) == (Character idB _ _ _ _) = idA == idB
+
+setHealth :: (Floating a) => Character -> a -> Character
+setHealth (Character id name _ pc items) x = (Character id name x pc items)
+
+changeHealth :: (Floating a) => Character -> a -> Character
+changeHealth (Character id name oldhp pc items) x = (Character id name (oldhp + x) pc items)
+
+
