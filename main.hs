2694ff332b9aa69f9f92407f25be181472158606
Elias Klaesener <elias.klaesener@uni-bielefeld.de>
2018-12-08 20:19:46 +0100

    Added some initialization procedures

--- main.hs
+++ main.hs
@@ -2,25 +2,40 @@ import Data.Array
 import System.IO
 import System.Random
 import qualified Movement as Mov
-import Character
+import qualified Character as Cha
 
 
 
-createMap :: IO Mov.Map
+createMap :: IO Mov.Map -- creates a new map with randomly locked rooms
 createMap = do gen <- newStdGen
                let randomBools = take Mov.numRooms (randoms gen :: [Bool])
                return (listArray ((Mov.lowBoundNS, Mov.lowBoundWE), (Mov.highBoundNS, Mov.highBoundWE)) randomBools)
 
 
+createCharacter :: [Integer] -> IO (Cha.Character, [Integer] -- creates an empty character with a unique id - that's why we have to pass that list around!
+createCharacter list = do gen <- newStdGen
+                     let id = head [x | x <- (randoms gen :: [Integer]), y <- list, x /= y]
+                     return ((Cha.Character id "" 100 False []), (id:list))
 
 
+createPlayer :: [Integer] -> IO (Cha.Character, [Integer]) -- creates a new player character
+createPlayer list = do (char, newList) <- createCharacter list
+                       print "How would you like to be called?"
+                       name <- getLine
+                       print ("Very well, " ++ name ++ " it is then.")
+                       let player = Cha.setPlayerCharacter True . Cha.changeName name $ char
+                       return (player, newList)
 
 
-
+-- play :: [Integer] -> [Cha.Character] -> Mov.Map -> ???
 
 
 main = do map <- createMap
---          player <- createPlayer
---          result <- play player map
+          (player, charIDList) <- createPlayer []
+          (char, charIDList) <- createCharacter charIDList
+--          result <- play charIDList [player] map
           print map
+          print player
+          print char
+          print charIDList
 --          putStrLn result
