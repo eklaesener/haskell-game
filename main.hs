51d806ea2c802792dfee97028376be3b9bde2be4
Elias Klaesener <elias.klaesener@uni-bielefeld.de>
2018-12-09 13:36:44 +0100

    Fixed infinite loop

--- main.hs
+++ main.hs
@@ -18,19 +18,19 @@ createCharacter map = do gen <- newStdGen
                          let randomIDs = randoms gen :: [Integer]
                          let id = head $ filter (\x -> not (Map.member x map)) randomIDs
                          let player = Cha.Character "" 100 False []
-                         let map = Map.insert id player map
-                         return (id, map)
+                         let newMap = Map.insert id player map
+                         return (id, newMap)
 
 
 createPlayer :: Map.Map Integer Cha.Character -> IO (Integer, Map.Map Integer Cha.Character) -- creates a new player character
-createPlayer map = do (id, newMap) <- createCharacter map
+createPlayer map = do (id, tempMap) <- createCharacter map
                       print "How would you like to be called?"
                       name <- getLine
                       print ("Very well, " ++ name ++ " it is then.")
-                      let (Just char) = Map.lookup id newMap
+                      let (Just char) = Map.lookup id tempMap
                       let player = Cha.setPlayerCharacter True . Cha.changeName name $ char
-                      let map = Map.insert id player newMap
-                      return (id, map)
+                      let newMap = Map.insert id player tempMap
+                      return (id, newMap)
 
 
 
@@ -48,11 +48,11 @@ type Game = (Mov.Map, Characters, CharMap, Items, ItemMap)
 
 
 main = do map <- createMap
-          (playerID, charMap) <- createPlayer Map.empty
-          (charID, charMap) <- createCharacter charMap
+          (playerID, charMap1) <- createPlayer Map.empty
+          (charID, charMap2) <- createCharacter charMap1
 --          result <- play charIDList [player] map
           print map
           print playerID
           print charID
-          print charMap
+          print charMap2
 --          putStrLn result
