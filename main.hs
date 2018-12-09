cd32be0706371162c7dee75b502ece24d4d2f9c1
Elias Klaesener <elias.klaesener@uni-bielefeld.de>
2018-12-09 16:05:43 +0100

    Added random start locations

--- main.hs
+++ main.hs
@@ -2,11 +2,27 @@ import Data.Array
 import System.IO
 import System.Random
 import Control.Monad.Random
-import qualified Data.Map as Map
+import qualified Data.Map.Strict as Map
 import qualified Movement as Mov
 import qualified Character as Cha
 
+-- making Pairs an instance of Random, taken from https://stackoverflow.com/questions/26674929/haskell-no-instance-for-random-point-arising-from-a-use-of-genrandom
+instance (Random x, Random y) => Random (x, y) where
+   randomR ((lo_x, lo_y), (hi_x, hi_y)) g = ((rand_x, rand_y), g'')
+      where (rand_x, g')  = randomR (lo_x, hi_x) g
+            (rand_y, g'') = randomR (lo_y, hi_y) g'
+   random g = ((rand_x, rand_y), g'')
+      where (rand_x, g') = random g
+            (rand_y, g'') = random g'
 
+-- making Directions an instance of Random
+instance Random Mov.Direction where
+   randomR (low, high) gen = (toEnum (fst (randomR (fromEnum low, fromEnum high) gen)), snd (randomR (fromEnum low, fromEnum high) gen))
+   random gen = randomR (Mov.West, Mov.East) gen
+
+
+
+-- creates weighted random lists, taken from https://stackoverflow.com/questions/8785577/generating-random-integers-with-given-probabilities
 weightedList :: RandomGen g => g -> [(a, Rational)] -> [a]
 weightedList gen weightList = evalRand m gen
    where m = sequence . repeat . fromList $ weightList
@@ -19,33 +35,44 @@ createMap = do gen <- newStdGen
                return (listArray ((Mov.lowBoundNS, Mov.lowBoundWE), (Mov.highBoundNS, Mov.highBoundWE)) randomBools)
 
 
-createCharacter :: Map.Map Integer Cha.Character -> IO (Integer, Map.Map Integer Cha.Character) -- creates an empty character with a unique id - that's why we have to pass that map around!
+createCharacter :: CharMap -> IO (Int, CharMap) -- creates an empty character with a unique id - that's why we have to pass that map around!
 createCharacter map = do gen <- newStdGen
-                         let randomIDs = randoms gen :: [Integer]
+                         let randomIDs = randoms gen :: [Int]
                          let id = head $ filter (\x -> not (Map.member x map)) randomIDs
                          let player = Cha.Character "" 100 False []
-                         let newMap = Map.insert id player map
+                         let pos = ((0,0), (0,0), Mov.West)
+                         let newMap = Map.insert id (player,pos) map
                          return (id, newMap)
 
 
-createPlayer :: Map.Map Integer Cha.Character -> IO (Integer, Map.Map Integer Cha.Character) -- creates a new player character
-createPlayer map = do (id, tempMap) <- createCharacter map
-                      print "How would you like to be called?"
-                      name <- getLine
-                      print ("Very well, " ++ name ++ " it is then.")
-                      let (Just char) = Map.lookup id tempMap
-                      let player = Cha.setPlayerCharacter True . Cha.changeName name $ char
-                      let newMap = Map.insert id player tempMap
-                      return (id, newMap)
+createPlayer :: Mov.Map -> CharMap -> IO (Int, CharMap) -- creates a new player character
+createPlayer roomMap charMap = do (id, tempMap) <- createCharacter charMap
+                                  print "How would you like to be called?"
+                                  name <- getLine
+                                  print ("Very well, " ++ name ++ " it is then.")
+                                  let (Just (char, pos)) = Map.lookup id tempMap
+                                  let player = Cha.setPlayerCharacter True . Cha.changeName name $ char
+                                  gen <- newStdGen
+                                  let allRooms = take Mov.numRooms $ randomRs ((Mov.lowBoundNS, Mov.lowBoundWE), (Mov.highBoundNS, Mov.highBoundWE)) gen :: [(Int,Int)]
+                                  let filteredRooms = filter (\x -> not (roomMap ! x)) allRooms
+                                  let newLoc = head filteredRooms
+                                  gen2 <- newStdGen
+                                  let innerLoc = head $ randomRs ((0,0), (Mov.roomSize,Mov.roomSize)) gen2
+                                  gen3 <- newStdGen
+                                  let dir = head $ randomRs (Mov.West, Mov.East) gen3
+                                  let newPos = (newLoc, innerLoc, dir)
+--                                  let newPos = head . filter () . take Mov.numRooms $ randomRs ((Mov.lowBoundNS, Mov.lowBoundWE), (Mov.highBoundNS, Mov.highBoundWE)) gen :: [(Int,Int)]
+                                  let newMap = Map.insert id (player,newPos) tempMap
+                                  return (id, newMap)
 
 
 
 
-type Characters = [Integer]
-type CharMap = Map.Map Integer Cha.Character
+type Characters = [Int]
+type CharMap = Map.Map Int (Cha.Character, Mov.Position)
 
-type Items = [Integer]
-type ItemMap = Map.Map Integer Cha.Item
+type Items = [Int]
+type ItemMap = Map.Map Int (Cha.Item, Mov.Position)
 
 type Game = (Mov.Map, Characters, CharMap, Items, ItemMap)
 
@@ -53,12 +80,14 @@ type Game = (Mov.Map, Characters, CharMap, Items, ItemMap)
 -- play :: Game -> ???
 
 
-main = do map <- createMap
-          (playerID, charMap1) <- createPlayer Map.empty
+main = do roomMap <- createMap
+          (playerID, charMap1) <- createPlayer roomMap Map.empty
           (charID, charMap2) <- createCharacter charMap1
 --          result <- play charIDList [player] map
-          print map
+          print roomMap
           print playerID
           print charID
           print charMap2
+          print (minBound :: Int)
+          print (maxBound :: Int)
 --          putStrLn result
