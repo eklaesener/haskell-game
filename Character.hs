7eb88c48b335572c331b9c36a9aaa641371aae5c
Elias Klaesener <elias.klaesener@uni-bielefeld.de>
2019-01-01 15:03:27 +0100

    Switched to record-based syntax in Character.hs

--- Character.hs
+++ Character.hs
@@ -12,26 +12,26 @@ data Character = Character { name :: String
 
 
 setHealth :: Float -> Character -> Character
-setHealth x (Character name _ pc items) = Character name x pc items
+setHealth x char = char { hp = x }
 
 changeHealth :: Float -> Character -> Character
-changeHealth x (Character name oldhp pc items) = Character name (oldhp + x) pc items
+changeHealth x char@(Character { hp = oldhp }) = char { hp = (oldhp + x) }
 
 setName :: String -> Character -> Character
-setName name (Character _ hp pc items) = Character name hp pc items
+setName newName char = char { name = newName }
 
 setPlayerCharacter :: Bool -> Character -> Character
-setPlayerCharacter pc (Character name hp _ items) = Character name hp pc items
+setPlayerCharacter x char = char { pc = x }
 
 pickupItem :: Item.Item -> Character -> Either String Character
-pickupItem item@(_, isInventory, _) (Character name hp pc oldItems)
+pickupItem item@(_, isInventory, _) char@(Character { items = oldItems })
    | not isInventory = Left "Item can't be picked up"
    | item `elem` oldItems = Left "Item already in Inventory"
-   | otherwise = Right $ Character name hp pc (item:oldItems)
+   | otherwise = Right (char { items = (item:oldItems) })
 
 dropItem :: Item.Item -> Character -> Either String Character
-dropItem item (Character name hp pc oldItems)
+dropItem item char@(Character { items = oldItems })
    | not (item `elem` oldItems) = Left "Item not in Inventory"
-   | otherwise = Right $ Character name hp pc (filter (/= item) oldItems)
+   | otherwise = Right (char { items = (filter (/= item) oldItems) })
 
 
